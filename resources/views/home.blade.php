f85005b71e3cb9d41e39e191d246aaa0d6bd5fa2
kapolandia <paykapo4@gmail.com>
2024-05-20 18:41:49 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.home-container')
+
+@section('header')
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="" srcset="">
+        </a>
+        <button class="navbar-toggler" type="button" @click="toggleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse my-header" >
+            <div class="navbar-nav">
+                <a class="nav-link" href="#" >CHARACTES</a>
+                <a class="nav-link" href="#">COMICS</a>
+                <a class="nav-link active" href="#">MOVIES</a>
+                <a class="nav-link" href="#">TV</a>
+                <a class="nav-link" href="#">GAMES</a>
+                <a class="nav-link" href="#">COLLECTIBLES</a>
+                <a class="nav-link" href="#">VIDEOS</a>
+                <a class="nav-link" href="#">FANS</a>
+                <a class="nav-link" href="#">NEWS</a>
+                <a class="nav-link" href="#">SHOP</a>
+                
+
+            </div>
+        </div>
+    </div>
+</nav>
+@endsection
+
+@section('main')
+<div class="jumbotron">
+
+</div>
+
+<div class=" p-5  my-section d-flex flex-column align-items-center justify-content-center">
+    <div class="d-flex align-items-center justify-content-center">
+        <div class="container row d-flex ">
+            @foreach ($comics as $comic)
+            <div class="col-2 ">
+                <div class="comics-container mt-2">
+                    <img src="{{ $comic['thumb'] }}" class="comics-img">
+                </div>
+                <div class="text-white pt-4">{{ strtoupper($comic['series']) }}</div>
+            </div>
+            @endforeach
+            </div>
+
+        </div>
+        <button class="btn btn-primary mx-auto">LOAD MORE</button>
+    </div>
+</div>
+
+{{-- <section>
+    <div class="container">
+        @foreach ($comics as $comic)
+        <div class="icon-card">
+            <img :src="getImageUrl(icon.src)">
+            <span class="icon-text">{{ icon.text }}</span>
+        </div>
+        @endforeach
+    </div>
+</section> --}}
+@endsection
+
+@section('footer')
+bub
+@endsection
